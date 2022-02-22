fe01ad5ef8989478da07a4a947107d54265a7c28
Daniel <mr_schliesing@hotmail.com>
2022-02-21 21:18:18 -0900

    learning state and binding and that sometimes swiftUI preview just doesn't work sometimes

---
+++ Scrumdinger/ThemePicker.swift
@@ -0,0 +1,26 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Daniel Schliesing on 2/21/22.
+//
+
+import SwiftUI
+
+struct ThemePicker: View {
+    @Binding var selection: Theme
+    var body: some View {
+        Picker("Theme", selection: $selection){
+            ForEach(Theme.allCases) { theme in
+                ThemeView(theme: theme)
+                    .tag(theme)
+            }
+        }
+    }
+}
+
+struct ThemePicker_Previews: PreviewProvider {
+    static var previews: some View {
+        ThemePicker(selection: .constant(.periwinkle))
+    }
+}
