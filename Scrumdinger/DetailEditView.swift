89efec9869a919076d6d7c21c10b5de791d4f2c0
Daniel <mr_schliesing@hotmail.com>
2022-02-22 21:09:12 -0900

    figured out my error, a function in the daily scrums model was missing. 'update'.

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -7,10 +7,8 @@
 
 import SwiftUI
 
-import SwiftUI
-
 struct DetailEditView: View {
-    @State private var data = DailyScrum.Data()
+    @Binding var data: DailyScrum.Data
     @State private var newAttendeeName = ""
     
     var body: some View {
@@ -45,6 +43,7 @@ struct DetailEditView: View {
                         }
                     }) {
                         Image(systemName: "plus.circle.fill")
+                            .accessibilityLabel("Add attendee")
                     }
                     .disabled(newAttendeeName.isEmpty)
                 }
@@ -55,6 +54,6 @@ struct DetailEditView: View {
 
 struct DetailEditView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailEditView()
+        DetailEditView(data: .constant(DailyScrum.sampleData[0].data))
     }
 }
--- Scrumdinger/ThemePicker.swift
+++ Scrumdinger/ThemePicker.swift
@@ -9,8 +9,9 @@ import SwiftUI
 
 struct ThemePicker: View {
     @Binding var selection: Theme
+    
     var body: some View {
-        Picker("Theme", selection: $selection){
+        Picker("Theme", selection: $selection) {
             ForEach(Theme.allCases) { theme in
                 ThemeView(theme: theme)
                     .tag(theme)
